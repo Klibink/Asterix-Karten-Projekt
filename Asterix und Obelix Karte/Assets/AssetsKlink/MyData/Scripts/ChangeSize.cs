4bb2d06ad2a2c6b7878df4602f20e7efc91daad5
Klibink <35121031+Klibink@users.noreply.github.com>
2019-02-21 14:51:09 +0100

    Repository erstellt und erste Szene added

---
+++ Asterix und Obelix Karte/Assets/AssetsKlink/MyData/Scripts/ChangeSize.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSize : MonoBehaviour {
+
+    public Renderer rend;
+    private Vector3 startScale = new Vector3(1, 1, 1);
+    private Vector3 maxScale = new Vector3(2, 2, 2);
+
+
+
+    // Use this for initialization
+    void Start () {
+        rend = GetComponent<Renderer>();
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (rend.enabled)
+        {
+            //if(transform.lossyScale!=maxScale) { 
+            StartCoroutine(startGrow());
+            //}
+        }
+        else
+        {
+
+            transform.localScale = startScale;
+        }
+
+	}
+
+    IEnumerator startGrow()
+    {
+        //transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(0.8f);
+        if (transform.lossyScale != maxScale)
+        {
+            transform.localScale += new Vector3(0.1F, 0.1f, 0.1f);
+        }
+        //SceneManager.LoadScene(nextScene);
+    }
+}
---
+++ Asterix und Obelix Karte/Assets/AssetsKlink/MyData/Scripts/ShowObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowObject : MonoBehaviour {
+
+    public ActivateLupe lupenObject;
+    private Renderer renderer;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseOver()
+    {
+        if (lupenObject.startMoving)
+        {
+            //gameObject.SetActive(true);
+            renderer.enabled = true;
+        }
+        
+    }
+
+    private void OnMouseExit()
+    {
+        if (lupenObject.startMoving)
+        {
+            //gameObject.SetActive(false);
+            renderer.enabled = false;
+        }
+    }
+}
