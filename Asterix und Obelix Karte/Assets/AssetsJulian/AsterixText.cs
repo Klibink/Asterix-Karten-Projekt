a59db3107fc2eb2c7f81d6ac5aed95eea49ec7ae
JKief <48298584+JKief@users.noreply.github.com>
2019-04-19 14:47:33 +0200

    assets

---
+++ Asterix und Obelix Karte/Assets/AssetsJulian/AsterixText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsterixText : MonoBehaviour
+{
+
+    public GameObject text;
+
+    private void OnTriggerEnter()
+    {
+        text.SetActive(true);
+
+    }
+
+    private void OnTriggerExit()
+    {
+        text.SetActive(false);
+
+
+    }
+
+}
---
+++ Asterix und Obelix Karte/Assets/AssetsJulian/CameraControllerRayCast.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CameraControllerRayCast : MonoBehaviour
+{
+    public Transform[] views;
+    public float transitionSpeed;
+    Transform currentView;
+    public float rayLength;
+    public LayerMask layermask;
+    public LayerMask layermask2;
+
+    void Start()
+    {
+        currentView = views[0];
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentView = views[0];
+        }
+
+
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, rayLength, layermask = 9))
+            {
+                currentView = views[1];
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, rayLength, layermask2))
+            {
+                currentView = views[2];
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * transitionSpeed);
+
+        Vector3 currentAngle = new Vector3(
+            Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentView.transform.rotation.eulerAngles.x, Time.deltaTime * transitionSpeed),
+            Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentView.transform.rotation.eulerAngles.y, Time.deltaTime * transitionSpeed),
+            Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentView.transform.rotation.eulerAngles.z, Time.deltaTime * transitionSpeed));
+
+        transform.eulerAngles = currentAngle;
+    }
+}
+
+   
