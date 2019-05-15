8a7b0aedd2c3b4817bd51e02ef3d0ead821642e0
lruehle <35702288+lruehle@users.noreply.github.com>
2019-05-15 19:37:25 +0200

    KameraAnimation
    Integrated Cam.Animation to startScene

--- Asterix und Obelix Karte/Assets/AssetsKlink/MyData/Scripts/CameraControllerNeu.cs
+++ Asterix und Obelix Karte/Assets/AssetsKlink/MyData/Scripts/CameraControllerNeu.cs
@@ -16,7 +16,7 @@ public class CameraControllerNeu : MonoBehaviour
 
     void Start()
     {
-        currentView = views[0];
+        currentView = views[1];
     }
 
     void Update()
@@ -67,13 +67,16 @@ public class CameraControllerNeu : MonoBehaviour
 
     private void LateUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * transitionSpeed);
+        if (currentView != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * transitionSpeed);
 
-        Vector3 currentAngle = new Vector3(
-            Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentView.transform.rotation.eulerAngles.x, Time.deltaTime * transitionSpeed),
-            Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentView.transform.rotation.eulerAngles.y, Time.deltaTime * transitionSpeed),
-            Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentView.transform.rotation.eulerAngles.z, Time.deltaTime * transitionSpeed));
+            Vector3 currentAngle = new Vector3(
+                Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentView.transform.rotation.eulerAngles.x, Time.deltaTime * transitionSpeed),
+                Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentView.transform.rotation.eulerAngles.y, Time.deltaTime * transitionSpeed),
+                Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentView.transform.rotation.eulerAngles.z, Time.deltaTime * transitionSpeed));
 
-        transform.eulerAngles = currentAngle;
+            transform.eulerAngles = currentAngle;
+        }
     }
 }
