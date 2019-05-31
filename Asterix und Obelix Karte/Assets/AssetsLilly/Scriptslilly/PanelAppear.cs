0b2aee3af41bf1f084d58b70c558b1fddd904e27
lruehle <35702288+lruehle@users.noreply.github.com>
2019-05-31 15:53:02 +0200

    Panels
    Panels auf rechter Buchseite erscheinen

---
+++ Asterix und Obelix Karte/Assets/AssetsLilly/Scriptslilly/PanelAppear.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PanelAppear : MonoBehaviour
+{
+    public SpriteRenderer Panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+       
+        if (other.tag == "Player")
+        {
+            Debug.Log(other.tag);
+            //  if (Panel != null)
+            //   {
+            Panel.enabled = true;
+         //   }     
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.tag == "Player")
+        {
+            Debug.Log(other.tag);
+            //  if (Panel != null)
+            //   {
+            Panel.enabled = false;
+            //   }     
+        }
+    }
+}
